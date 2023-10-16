c0fa7b13e6e7b4e9af28f4dd246769cab07fb945
DOYG-N-DTYD <miroslawzborowski1105@gmail.com>
2023-10-16 14:00:44 +0200

    additional dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,11 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0") // work with internet requests
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0") // parsing internet responses form requests
+    implementation("io.coil-kt:coil-compose:2.4.0") // download images
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -66,4 +71,6 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+
 }
